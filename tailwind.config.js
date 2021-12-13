c4dd586d8573a8a0b93161dc4d978f63e4dc2da1
Billy Hawkes <70971818+billyhawkes@users.noreply.github.com>
2021-12-13 14:51:21 -0500

    Initial setup with name and socials

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 module.exports = {
 	content: [
-		"./pages/**/*.{js,ts,jsx,tsx}",
-		"./components/**/*.{js,ts,jsx,tsx}",
+		"./src/pages/**/*.{js,ts,jsx,tsx}",
+		"./src/components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
 		extend: {},
+		colors: {
+			primary: "#425FA9",
+			background: "#25272F",
+			white: "#FFFFFF",
+		},
+		fontFamily: {
+			display: ['"Josefin Sans"', "sans"],
+			body: ['"Open Sans"', "sans"],
+		},
 	},
 	plugins: [],
 };
