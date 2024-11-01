e3efccbd04cbb088b82e64270b9eb14f425dd533
Billy Hawkes <billyhawkes02@gmail.com>
2024-11-01 16:31:05 -0400

    feat: added production image storage in r2

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20241101_171242 from './20241101_171242';
-
-export const migrations = [
-  {
-    up: migration_20241101_171242.up,
-    down: migration_20241101_171242.down,
-    name: '20241101_171242'
-  },
-];
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,39 +1,53 @@
 // storage-adapter-import-placeholder
-import { sqliteAdapter } from '@payloadcms/db-sqlite'
-import { lexicalEditor } from '@payloadcms/richtext-lexical'
-import path from 'path'
-import { buildConfig } from 'payload'
-import sharp from 'sharp'
-import { fileURLToPath } from 'url'
+import { sqliteAdapter } from "@payloadcms/db-sqlite";
+import { lexicalEditor } from "@payloadcms/richtext-lexical";
+import path from "path";
+import { buildConfig } from "payload";
+import sharp from "sharp";
+import { fileURLToPath } from "url";
 
-import { Media } from './collections/Media'
-import { Projects } from './collections/Projects'
-import { Users } from './collections/Users'
+import { s3Storage } from "@payloadcms/storage-s3";
+import { Media } from "./collections/Media";
+import { Projects } from "./collections/Projects";
+import { Users } from "./collections/Users";
 
-const filename = fileURLToPath(import.meta.url)
-const dirname = path.dirname(filename)
+const filename = fileURLToPath(import.meta.url);
+const dirname = path.dirname(filename);
 
 export default buildConfig({
-  admin: {
-    user: Users.slug,
-    importMap: {
-      baseDir: path.resolve(dirname),
-    },
-  },
-  collections: [Users, Media, Projects],
-  editor: lexicalEditor(),
-  secret: process.env.PAYLOAD_SECRET || '',
-  typescript: {
-    outputFile: path.resolve(dirname, 'payload-types.ts'),
-  },
-  db: sqliteAdapter({
-    client: {
-      url: process.env.DATABASE_URL || '',
-      authToken: process.env.DATABASE_TOKEN || '',
-    },
-  }),
-  sharp,
-  plugins: [
-    // storage-adapter-placeholder
-  ],
-})
+	admin: {
+		user: Users.slug,
+		importMap: {
+			baseDir: path.resolve(dirname),
+		},
+	},
+	collections: [Users, Media, Projects],
+	editor: lexicalEditor(),
+	secret: process.env.PAYLOAD_SECRET || "",
+	typescript: {
+		outputFile: path.resolve(dirname, "payload-types.ts"),
+	},
+	db: sqliteAdapter({
+		client: {
+			url: process.env.DATABASE_URL || "",
+			authToken: process.env.DATABASE_TOKEN || "",
+		},
+	}),
+	sharp,
+	plugins: [
+		s3Storage({
+			collections: {
+				media: true,
+			},
+			bucket: process.env.R2_BUCKET || "",
+			config: {
+				credentials: {
+					accessKeyId: process.env.R2_ACCESS_KEY_ID || "",
+					secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || "",
+				},
+				// region: process.env.R2_REGION || "",
+				endpoint: process.env.R2_ENDPOINT || "",
+			},
+		}),
+	],
+});
